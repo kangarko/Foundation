b133f47172ea290ac10a1b3c09cb8cfd12c21e8f
kangarko <kangaro.matej@azet.sk>
2019-06-26 10:34:39 +0200

    Add entity tracking methods

--- src/main/java/org/mineacademy/fo/EntityUtil.java
+++ src/main/java/org/mineacademy/fo/EntityUtil.java
@@ -12,6 +12,8 @@ import org.bukkit.entity.Player;
 import org.bukkit.entity.Projectile;
 import org.bukkit.entity.Slime;
 import org.bukkit.entity.Wolf;
+import org.bukkit.scheduler.BukkitRunnable;
+import org.mineacademy.fo.exception.FoException;
 
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
@@ -81,5 +83,84 @@ public final class EntityUtil {
 				entity instanceof Item ||
 				entity instanceof Projectile ||
 				entity instanceof ExperienceOrb;
+	}
+
+	/**
+	 * Runs a timer task with the tick period of 1 and will trigger your hit listener
+	 * when the given entity isOnGround. If the entity gets removed before it hits
+	 * the ground, nothing is called
+	 *
+	 * If the entity still flies after 20 seconds, nothing is called
+	 *
+	 * @param entity
+	 * @param hitGroundListener
+	 */
+	public static void trackFalling(Entity entity, Runnable hitGroundListener) {
+		track(entity, 20 * 20, null, hitGroundListener);
+	}
+
+	/**
+	 * Runs a timer task with the tick period of 1 and will trigger your fly listener
+	 * each tick until entity is either removed or isOnGround
+	 *
+	 * If the entity still flies after 20 seconds, nothing is called
+	 *
+	 * @param entity
+	 * @param flyListener
+	 */
+	public static void trackFlying(Entity entity, Runnable flyListener) {
+		track(entity, 20 * 20, flyListener, null);
+	}
+
+	/**
+	 * Runs a timer task with the tick period of 1 and will trigger your hit listener
+	 * when the given entity isOnGround. If the entity gets removed before it hits
+	 * the ground, nothing is called
+	 *
+	 * The fly listener is called every tick
+	 *
+	 * @param entity
+	 * @param timeoutTicks
+	 * @param flyListener
+	 * @param hitGroundListener
+	 */
+	public static void track(Entity entity, int timeoutTicks, Runnable flyListener, Runnable hitGroundListener) {
+		if (flyListener == null && hitGroundListener == null)
+			throw new FoException("Cannot track entity with fly and hit listeners on null!");
+
+		Common.runTimer(1, new BukkitRunnable() {
+
+			private int elapsedTicks = 0;
+
+			@Override
+			public void run() {
+
+				// Cancel after the given timeout to save performance
+				if (elapsedTicks++ > timeoutTicks) {
+					cancel();
+
+					return;
+				}
+
+				// Cancel when invalid
+				if (entity == null || entity.isDead() || !entity.isValid()) {
+					cancel();
+
+					return;
+				}
+
+				// Run the hit listener
+				if (entity.isOnGround()) {
+					if (hitGroundListener != null)
+						hitGroundListener.run();
+
+					cancel();
+
+				} else {
+					if (flyListener != null)
+						flyListener.run();
+				}
+			}
+		});
 	}
 }
