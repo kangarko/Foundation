2448c759c680f31f5278850a1056013735378b44
kangarko <kangaro.matej@azet.sk>
2019-08-29 05:34:47 +0200

    Improve SimpleListener

--- src/main/java/org/mineacademy/fo/event/SimpleListener.java
+++ src/main/java/org/mineacademy/fo/event/SimpleListener.java
@@ -1,11 +1,18 @@
 package org.mineacademy.fo.event;
 
 import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Cancellable;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventException;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.plugin.EventExecutor;
+import org.mineacademy.fo.Common;
+import org.mineacademy.fo.PlayerUtil;
+import org.mineacademy.fo.Valid;
+import org.mineacademy.fo.debug.LagCatcher;
+import org.mineacademy.fo.exception.EventHandledException;
 import org.mineacademy.fo.plugin.SimplePlugin;
 
 import lombok.RequiredArgsConstructor;
@@ -21,7 +28,7 @@ public abstract class SimpleListener<T extends Event> implements Listener, Event
 	/**
 	 * The event we are listening to
 	 */
-	private final Class<T> event;
+	private final Class<T> eventClass;
 
 	/**
 	 * The event priority
@@ -32,6 +39,11 @@ public abstract class SimpleListener<T extends Event> implements Listener, Event
 	 * Shall we ignore cancelled events down the pipeline?
 	 */
 	private final boolean ignoreCancelled;
+
+	/**
+	 * The run event temporary placeholder
+	 */
+	private T event;
 
 	/**
 	 * Creates a new listener using the normal priority
@@ -55,7 +67,32 @@ public abstract class SimpleListener<T extends Event> implements Listener, Event
 
 	@Override
 	public final void execute(Listener listener, Event event) throws EventException {
-		execute((T) event);
+		final String logName = event.getEventName() + " at " + priority + "(ignore cancelled = " + ignoreCancelled + ")";
+
+		LagCatcher.start(logName);
+
+		try {
+			this.event = (T) event;
+
+			execute((T) event);
+
+		} catch (final EventHandledException ex) {
+			final String[] messages = ex.getMessages();
+			final boolean cancelled = ex.isCancelled();
+
+			final Player player = findPlayer((T) event);
+
+			if (messages != null && player != null)
+				Common.tell(player, messages);
+
+			if (cancelled && event instanceof Cancellable)
+				((Cancellable) event).setCancelled(true);
+
+		} finally {
+			LagCatcher.end(logName);
+
+			this.event = null;
+		}
 	}
 
 	/**
@@ -65,7 +102,73 @@ public abstract class SimpleListener<T extends Event> implements Listener, Event
 	 */
 	public abstract void execute(T event);
 
+	/**
+	 * Return a player from this event, null if none,
+	 * used for messaging
+	 *
+	 * @param event
+	 * @return
+	 */
+	protected Player findPlayer(T event) {
+		return null;
+	}
+
+	/**
+	 * If the condition is false, stop your code from further execution, cancel the event and
+	 * send the player a false message (see {@link #findPlayer(Event)})
+	 *
+	 * @param condition
+	 * @param falseMessages
+	 */
+	protected final void checkBoolean(boolean condition, String... falseMessages) {
+		if (!condition)
+			throw new EventHandledException(true, falseMessages);
+	}
+
+	/**
+	 * Stop code from executing and send the player a message (see {@link #findPlayer(Event)})
+	 * when he lacks the given permission
+	 *
+	 * @param permission
+	 * @param falseMessage
+	 */
+	protected final void checkPerm(String permission, String falseMessage) {
+		final Player player = findPlayer(event);
+		Valid.checkNotNull(player, "Player cannot be null for " + event.getEventName() + "!");
+
+		if (!PlayerUtil.hasPerm(player, permission))
+			throw new EventHandledException(true, falseMessage.replace("{permission}", permission));
+	}
+
+	/**
+	 * Cancel the event and send the player a message (see {@link #findPlayer(Event)})
+	 *
+	 * @param messages
+	 */
+	protected final void cancel(String... messages) {
+		throw new EventHandledException(true, messages);
+	}
+
+	/**
+	 * Cancel this event
+	 */
+	protected final void cancel() {
+		throw new EventHandledException(true);
+	}
+
+	/**
+	 * Return code execution and send messages
+	 *
+	 * @param messages
+	 */
+	protected final void returnTell(String... messages) {
+		throw new EventHandledException(false, messages);
+	}
+
+	/**
+	 * A shortcut for registering this event in Bukkit
+	 */
 	public final void register() {
-		Bukkit.getPluginManager().registerEvent(event, this, priority, this, SimplePlugin.getInstance(), ignoreCancelled);
+		Bukkit.getPluginManager().registerEvent(eventClass, this, priority, this, SimplePlugin.getInstance(), ignoreCancelled);
 	}
 }
