939e58296a15f3144a8cbed2431cbca772d764ff
kangarko <kangaro.matej@azet.sk>
2020-03-01 13:29:58 +0100

    Make tuple serialize

--- src/main/java/org/mineacademy/fo/model/Tuple.java
+++ src/main/java/org/mineacademy/fo/model/Tuple.java
@@ -1,12 +1,14 @@
 package org.mineacademy.fo.model;
 
+import org.mineacademy.fo.collection.SerializedMap;
+
 import lombok.Data;
 
 /**
  * Simple tuple for key-value pairs
  */
 @Data
-public final class Tuple<K, V> {
+public final class Tuple<K, V> implements ConfigSerializable {
 
 	/**
 	 * The key
@@ -17,4 +19,27 @@ public final class Tuple<K, V> {
 	 * The value
 	 */
 	private final V value;
+
+	/**
+	 * @see org.mineacademy.fo.model.ConfigSerializable#serialize()
+	 */
+	@Override
+	public SerializedMap serialize() {
+		return SerializedMap.ofArray("Key", key, "Value", value);
+	}
+
+	/**
+	 * Transform (speculatively) config section to tuple
+	 *
+	 * @param <K>
+	 * @param <V>
+	 * @param map
+	 * @return
+	 */
+	public static <K, V> Tuple<K, V> deserialize(SerializedMap map) {
+		final K key = (K) map.getObject("Key");
+		final V value = (V) map.getObject("Value");
+
+		return new Tuple<>(key, value);
+	}
 }
