3f9304355fdb61897a4388b398436737df7bcdd2
Ladislav Proc <53174757+lackoSK@users.noreply.github.com>
2020-05-16 10:33:23 +0200

    Added Silent_Startup

--- src/main/java/org/mineacademy/fo/model/ConfigItems.java
+++ src/main/java/org/mineacademy/fo/model/ConfigItems.java
@@ -11,6 +11,7 @@ import org.apache.commons.lang.WordUtils;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.FileUtil;
 import org.mineacademy.fo.Valid;
+import org.mineacademy.fo.settings.SimpleSettings;
 import org.mineacademy.fo.settings.YamlConfig;
 
 import lombok.NonNull;
@@ -96,7 +97,9 @@ public final class ConfigItems<T extends YamlConfig> {
 
 			// Register
 			loadedItems.add(item);
-			Common.log("[+] Loaded " + type + " " + item.getName());
+
+			if (!SimpleSettings.SILENT_STARTUP)
+				Common.log("[+] Loaded " + type + " " + item.getName());
 
 			return item;
 
