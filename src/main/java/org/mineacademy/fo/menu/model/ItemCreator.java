bd8c7e15ccd8d67be5261e243b93831cdc344a57
Andrew Wong <42793301+md5sha256@users.noreply.github.com>
2020-08-09 11:31:00 +0200

    Replace reflection of AuthLib with imports in ItemCreator#ofSkullHashx

--- src/main/java/org/mineacademy/fo/menu/model/ItemCreator.java
+++ src/main/java/org/mineacademy/fo/menu/model/ItemCreator.java
@@ -1,12 +1,11 @@
 package org.mineacademy.fo.menu.model;
 
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collection;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
+import com.mojang.authlib.GameProfile;
+import com.mojang.authlib.properties.Property;
+import com.mojang.authlib.properties.PropertyMap;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.EntityType;
@@ -412,13 +411,17 @@ public final class ItemCreator {
 		final ItemStack head = new ItemStack(CompMaterial.PLAYER_HEAD.getMaterial(), 1, (short) 3);
 		final SkullMeta meta = (SkullMeta) head.getItemMeta();
 
-		final Object gameProfile = ReflectionUtil.instantiate(ReflectionUtil.lookupClass("com.mojang.authlib.properties.Property"), UUID.randomUUID(), "");
-		final Object property = ReflectionUtil.instantiate(ReflectionUtil.lookupClass("com.mojang.authlib.properties.Property"), "textures", hash);
-		final Object properties = ReflectionUtil.invoke("getProperties", gameProfile);
+		GameProfile gameProfile = new GameProfile(UUID.randomUUID(), "");
+		Property property = new Property("textures", hash);
+		PropertyMap properties = gameProfile.getProperties();
+		properties.put("textures", property);
+		//final Object gameProfile = ReflectionUtil.instantiate(ReflectionUtil.lookupClass("com.mojang.authlib.properties.Property"), UUID.randomUUID(), "");
+		//final Object property = ReflectionUtil.instantiate(ReflectionUtil.lookupClass("com.mojang.authlib.properties.Property"), "textures", hash);
+		//final Object properties = ReflectionUtil.invoke("getProperties", gameProfile);
 
 		ReflectionUtil.invoke("put", properties, "textures", property);
 
-		Field profileField = null;
+		Field profileField;
 
 		try {
 			profileField = meta.getClass().getDeclaredField("profile");
