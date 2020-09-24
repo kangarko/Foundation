d87bd2679aceb9a27d2be6a98d1a5145a7c94217
kangarko <matej@matejpacan.com>
2020-09-24 20:53:13 +0200

    Turn CompBarX classes into keyed for saving

--- src/main/java/org/mineacademy/fo/remain/CompBarColor.java
+++ src/main/java/org/mineacademy/fo/remain/CompBarColor.java
@@ -1,14 +1,43 @@
 package org.mineacademy.fo.remain;
 
+import org.mineacademy.fo.Common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * A wrapper for BarColor from bukkit
  */
+@RequiredArgsConstructor
 public enum CompBarColor {
-	PINK,
-	BLUE,
-	RED,
-	GREEN,
-	YELLOW,
-	PURPLE,
-	WHITE;
+
+	PINK("PINK"),
+	BLUE("BLUE"),
+	RED("RED"),
+	GREEN("GREEN"),
+	YELLOW("YELLOW"),
+	PURPLE("PURPLE"),
+	WHITE("WHITE");
+
+	@Getter
+	private final String key;
+
+	/**
+	 * Attempt to load CompBarColor from the given key
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static CompBarColor fromKey(String key) {
+		for (final CompBarColor mode : values())
+			if (mode.key.equalsIgnoreCase(key))
+				return mode;
+
+		throw new IllegalArgumentException("No such CompBarColor: " + key + ". Available: " + Common.join(values()));
+	}
+
+	@Override
+	public String toString() {
+		return this.key;
+	}
 }
--- src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
+++ src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
@@ -1,32 +1,60 @@
 package org.mineacademy.fo.remain;
 
+import org.mineacademy.fo.Common;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
 /**
  * A wrapper for BarStyle from bukkit
  */
+@RequiredArgsConstructor
 public enum CompBarStyle {
 
 	/**
 	 * Makes the boss bar solid (no segments)
 	 */
-	SOLID,
+	SOLID("SOLID"),
 
 	/**
 	 * Splits the boss bar into 6 segments
 	 */
-	SEGMENTED_6,
+	SEGMENTED_6("SEGMENTED_6"),
 
 	/**
 	 * Splits the boss bar into 10 segments
 	 */
-	SEGMENTED_10,
+	SEGMENTED_10("SEGMENTED_10"),
 
 	/**
 	 * Splits the boss bar into 12 segments
 	 */
-	SEGMENTED_12,
+	SEGMENTED_12("SEGMENTED_12"),
 
 	/**
 	 * Splits the boss bar into 20 segments
 	 */
-	SEGMENTED_20,
+	SEGMENTED_20("SEGMENTED_20");
+
+	@Getter
+	private final String key;
+
+	/**
+	 * Attempt to load CompBarStyle from the given key
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static CompBarStyle fromKey(String key) {
+		for (final CompBarStyle mode : values())
+			if (mode.key.equalsIgnoreCase(key))
+				return mode;
+
+		throw new IllegalArgumentException("No such CompBarStyle: " + key + ". Available: " + Common.join(values()));
+	}
+
+	@Override
+	public String toString() {
+		return this.key;
+	}
 }
