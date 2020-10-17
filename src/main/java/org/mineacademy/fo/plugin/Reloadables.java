390f4ab0884bfcbc2b8ec279d0f71f7e6c65930c
kangarko <matej@matejpacan.com>
2020-10-17 13:25:10 +0200

    Add ability to automatically reload command groups

--- src/main/java/org/mineacademy/fo/plugin/Reloadables.java
+++ src/main/java/org/mineacademy/fo/plugin/Reloadables.java
@@ -1,10 +1,13 @@
 package org.mineacademy.fo.plugin;
 
+import java.util.List;
+
 import org.bukkit.event.Event;
 import org.bukkit.event.HandlerList;
 import org.bukkit.event.Listener;
 import org.mineacademy.fo.Common;
 import org.mineacademy.fo.collection.StrictList;
+import org.mineacademy.fo.command.SimpleCommandGroup;
 import org.mineacademy.fo.event.SimpleListener;
 
 /**
@@ -18,6 +21,11 @@ final class Reloadables {
 	 */
 	private final StrictList<Listener> listeners = new StrictList<>();
 
+	/**
+	 * A list of registered command groups
+	 */
+	private final StrictList<SimpleCommandGroup> commandGroups = new StrictList<>();
+
 	// -------------------------------------------------------------------------------------------
 	// Main
 	// -------------------------------------------------------------------------------------------
@@ -30,6 +38,11 @@ final class Reloadables {
 			HandlerList.unregisterAll(listener);
 
 		listeners.clear();
+
+		for (final SimpleCommandGroup commandGroup : commandGroups)
+			commandGroup.unregister();
+
+		commandGroups.clear();
 	}
 
 	// -------------------------------------------------------------------------------------------
@@ -57,5 +70,34 @@ final class Reloadables {
 		listener.register();
 
 		listeners.add(listener);
+	}
+
+	// -------------------------------------------------------------------------------------------
+	// Command groups
+	// -------------------------------------------------------------------------------------------
+
+	/**
+	 * Register the given command group
+	 *
+	 * @param label
+	 * @param aliases
+	 * @param group
+	 */
+	void registerCommands(final String label, final List<String> aliases, final SimpleCommandGroup group) {
+		group.register(label, aliases);
+
+		commandGroups.add(group);
+	}
+
+	/**
+	 * Register the given command group
+	 *
+	 * @param labelAndAliases
+	 * @param group
+	 */
+	void registerCommands(final StrictList<String> labelAndAliases, final SimpleCommandGroup group) {
+		group.register(labelAndAliases);
+
+		commandGroups.add(group);
 	}
 }
