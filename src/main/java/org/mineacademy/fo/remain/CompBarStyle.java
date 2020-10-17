9aaca33449f13172752753e75a511d83fa8e1957
kangarko <matej@matejpacan.com>
2020-10-17 13:24:08 +0200

    Add shortKey to CompBarStyle

--- src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
+++ src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
@@ -14,30 +14,33 @@ public enum CompBarStyle {
 	/**
 	 * Makes the boss bar solid (no segments)
 	 */
-	SOLID("SOLID"),
+	SOLID("SOLID", "SOLID"),
 
 	/**
 	 * Splits the boss bar into 6 segments
 	 */
-	SEGMENTED_6("SEGMENTED_6"),
+	SEGMENTED_6("SEGMENTED_6", "SEG6"),
 
 	/**
 	 * Splits the boss bar into 10 segments
 	 */
-	SEGMENTED_10("SEGMENTED_10"),
+	SEGMENTED_10("SEGMENTED_10", "SEG10"),
 
 	/**
 	 * Splits the boss bar into 12 segments
 	 */
-	SEGMENTED_12("SEGMENTED_12"),
+	SEGMENTED_12("SEGMENTED_12", "SEG12"),
 
 	/**
 	 * Splits the boss bar into 20 segments
 	 */
-	SEGMENTED_20("SEGMENTED_20");
+	SEGMENTED_20("SEGMENTED_20", "SEG20");
 
 	@Getter
 	private final String key;
+
+	@Getter
+	private final String shortKey;
 
 	/**
 	 * Attempt to load CompBarStyle from the given key
@@ -47,7 +50,7 @@ public enum CompBarStyle {
 	 */
 	public static CompBarStyle fromKey(String key) {
 		for (final CompBarStyle mode : values())
-			if (mode.key.equalsIgnoreCase(key))
+			if (mode.key.equalsIgnoreCase(key) || mode.shortKey.equalsIgnoreCase(key))
 				return mode;
 
 		throw new IllegalArgumentException("No such CompBarStyle: " + key + ". Available: " + Common.join(values()));
