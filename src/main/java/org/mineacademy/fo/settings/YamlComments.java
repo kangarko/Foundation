b73de578c9507d3c376c698489adbf04beb0df3a
kangarko <git@kangarko.com>
2020-11-05 19:25:38 +0100

    World's most important commit (cancer cured)

--- src/main/java/org/mineacademy/fo/settings/YamlComments.java
+++ src/main/java/org/mineacademy/fo/settings/YamlComments.java
@@ -109,7 +109,7 @@ public final class YamlComments {
 
 			backupConfig.save(backupFile);
 
-			Common.log("&6Warning: The following entries in " + diskFile.getName() + " are unused and were moved into " + backupFile.getName() + ": " + removedKeys.keySet());
+			Common.log("&cWarning: The following entries in " + diskFile.getName() + " are unused and were moved into " + backupFile.getName() + ": " + removedKeys.keySet());
 		}
 
 		final Yaml yaml = new Yaml();
@@ -303,10 +303,10 @@ public final class YamlComments {
 				/*for (final String ignoredSection : ignoredSections) {
 					if (keyBuilder.toString().equals(ignoredSection)) {
 						final Object value = oldConfig.get(keyBuilder.toString());
-				
+
 						if (value instanceof ConfigurationSection)
 							appendSection(builder, (ConfigurationSection) value, new StringBuilder(getPrefixSpaces(lastLineIndentCount)), yaml);
-				
+
 						continue outer;
 					}
 				}*/
