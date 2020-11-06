e5dde07c2dbd80ce93960e9294bd2e7b9895c38d
kangarko <git@kangarko.com>
2020-11-06 10:27:55 +0100

    Make eventhandledexception extends commandexception

--- src/main/java/org/mineacademy/fo/exception/EventHandledException.java
+++ src/main/java/org/mineacademy/fo/exception/EventHandledException.java
@@ -6,15 +6,9 @@ import lombok.Getter;
  * Represents a silent exception thrown then handling events,
  * this will only send the event player a message
  */
-public final class EventHandledException extends RuntimeException {
+public final class EventHandledException extends CommandException {
 
 	private static final long serialVersionUID = 1L;
-
-	/**
-	 * The messages to send to the command sender
-	 */
-	@Getter
-	private final String[] messages;
 
 	/**
 	 * Should we cancel this event?
@@ -32,9 +26,8 @@ public final class EventHandledException extends RuntimeException {
 	 * @param messages
 	 */
 	public EventHandledException(boolean cancelled, String... messages) {
-		super("");
+		super(messages);
 
 		this.cancelled = cancelled;
-		this.messages = messages;
 	}
 }
