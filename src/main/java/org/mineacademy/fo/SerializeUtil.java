a6d4063f787a84279402b77df5edb878b9ba295d
Matej Pacan <matej@matejpacan.com>
2021-02-03 10:09:29 +0100

    Add serialize/deserialize manual methods for location with decimal support

--- src/main/java/org/mineacademy/fo/SerializeUtil.java
+++ src/main/java/org/mineacademy/fo/SerializeUtil.java
@@ -180,12 +180,24 @@ public final class SerializeUtil {
 
 	/**
 	 * Converts a {@link Location} into "world x y z yaw pitch" string
+	 * Decimals not supported, use {@link #deserializeLocationD(Object)} for them
 	 *
 	 * @param loc
 	 * @return
 	 */
 	public static String serializeLoc(final Location loc) {
 		return loc.getWorld().getName() + " " + loc.getBlockX() + " " + loc.getBlockY() + " " + loc.getBlockZ() + (loc.getPitch() != 0F || loc.getYaw() != 0F ? " " + Math.round(loc.getYaw()) + " " + Math.round(loc.getPitch()) : "");
+	}
+
+	/**
+	 * Converts a {@link Location} into "world x y z yaw pitch" string with decimal support
+	 * Unused, you have to call this in your save() method otherwise we remove decimals and use the above method
+	 *
+	 * @param loc
+	 * @return
+	 */
+	public static String serializeLocD(final Location loc) {
+		return loc.getWorld().getName() + " " + loc.getX() + " " + loc.getY() + " " + loc.getZ() + (loc.getPitch() != 0F || loc.getYaw() != 0F ? " " + loc.getYaw() + " " + loc.getPitch() : "");
 	}
 
 	/**
@@ -373,6 +385,7 @@ public final class SerializeUtil {
 
 	/**
 	 * Converts a string into location, see {@link #deserializeLocation(Object)} for how strings are saved
+	 * Decimals not supported, use {@link #deserializeLocationD(Object)} to use them
 	 *
 	 * @param raw
 	 * @return
@@ -395,6 +408,37 @@ public final class SerializeUtil {
 			throw new InvalidWorldException("Location with invalid world '" + world + "': " + raw + " (Doesn't exist)", world);
 
 		final int x = Integer.parseInt(parts[1]), y = Integer.parseInt(parts[2]), z = Integer.parseInt(parts[3]);
+		final float yaw = Float.parseFloat(parts.length == 6 ? parts[4] : "0"), pitch = Float.parseFloat(parts.length == 6 ? parts[5] : "0");
+
+		return new Location(bukkitWorld, x, y, z, yaw, pitch);
+	}
+
+	/**
+	 * Converts a string into a location with decimal support
+	 * Unused but you can use this for your own parser storing exact decimals
+	 * 
+	 * @param raw
+	 * @return
+	 */
+	public static Location deserializeLocationD(Object raw) {
+		if (raw == null)
+			return null;
+
+		if (raw instanceof Location)
+			return (Location) raw;
+
+		raw = raw.toString().replace("\"", "");
+
+		final String[] parts = raw.toString().contains(", ") ? raw.toString().split(", ") : raw.toString().split(" ");
+		Valid.checkBoolean(parts.length == 4 || parts.length == 6, "Expected location (String) but got " + raw.getClass().getSimpleName() + ": " + raw);
+
+		final String world = parts[0];
+		final World bukkitWorld = Bukkit.getWorld(world);
+
+		if (bukkitWorld == null)
+			throw new InvalidWorldException("Location with invalid world '" + world + "': " + raw + " (Doesn't exist)", world);
+
+		final double x = Double.parseDouble(parts[1]), y = Double.parseDouble(parts[2]), z = Double.parseDouble(parts[3]);
 		final float yaw = Float.parseFloat(parts.length == 6 ? parts[4] : "0"), pitch = Float.parseFloat(parts.length == 6 ? parts[5] : "0");
 
 		return new Location(bukkitWorld, x, y, z, yaw, pitch);
