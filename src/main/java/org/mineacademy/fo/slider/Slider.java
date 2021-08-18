6b29ea57805738256ef29c5b5e2bc59afc8da5a1
Matej Pacan <matej@matejpacan.com>
2021-08-18 21:40:34 +0200

    Add sliders

---
+++ src/main/java/org/mineacademy/fo/slider/Slider.java
@@ -0,0 +1,17 @@
+package org.mineacademy.fo.slider;
+
+/**
+ * Represents a slider for animating text or items.
+ *
+ * A slider takes in a list of items (or a string) and
+ * then moves them in the precreated direction using the {@link #next()} method.
+ */
+public interface Slider<T> {
+
+	/**
+	 * Move to the next item in the list.
+	 *
+	 * @return
+	 */
+	T next();
+}
