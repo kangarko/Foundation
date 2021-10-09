c223d780b3c2b6fb399a3b3c31312e7c33fd56db
Matej Pacan <matej@matejpacan.com>
2021-10-09 07:24:50 +0200

    Create CompEquipmentSlot.java

---
+++ src/main/java/org/mineacademy/fo/remain/CompEquipmentSlot.java
@@ -0,0 +1,133 @@
+package org.mineacademy.fo.remain;
+
+import javax.annotation.Nullable;
+
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.inventory.EntityEquipment;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.mineacademy.fo.MinecraftVersion;
+import org.mineacademy.fo.MinecraftVersion.V;
+import org.mineacademy.fo.Valid;
+import org.mineacademy.fo.exception.FoException;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Represents {@link EquipmentSlot}
+ */
+@RequiredArgsConstructor
+public enum CompEquipmentSlot {
+
+	HAND("HAND"),
+	/**
+	 * Requires Minecraft 1.9+
+	 */
+	OFF_HAND("OFF_HAND"),
+	HEAD("HEAD"),
+	CHEST("CHEST"),
+	LEGS("LEGS"),
+	FEET("FEET");
+
+	/**
+	 * The localizable key
+	 */
+	@Getter
+	private final String key;
+
+	/**
+	 * Applies this equipment slot to the given entity with the given item
+	 *
+	 * @param entity
+	 * @param item
+	 */
+	public void applyTo(LivingEntity entity, ItemStack item) {
+		this.applyTo(entity, null);
+	}
+
+	/**
+	 * Applies this equipment slot to the given entity with the given item,
+	 * and optional drop chance from 0 to 1.0
+	 *
+	 * @param entity
+	 * @param item
+	 * @param dropChance
+	 */
+	public void applyTo(LivingEntity entity, ItemStack item, @Nullable Double dropChance) {
+		final EntityEquipment equip = entity.getEquipment();
+
+		switch (this) {
+
+			case HAND:
+				equip.setItemInHand(item);
+
+				if (dropChance != null)
+					equip.setItemInHandDropChance(dropChance.floatValue());
+
+				break;
+
+			case OFF_HAND:
+				Valid.checkBoolean(MinecraftVersion.atLeast(V.v1_9), "Setting off hand item requires Minecraft 1.9+");
+
+				equip.setItemInOffHand(item);
+
+				if (dropChance != null)
+					equip.setItemInOffHandDropChance(dropChance.floatValue());
+
+				break;
+
+			case HEAD:
+				equip.setHelmet(item);
+
+				if (dropChance != null)
+					equip.setHelmetDropChance(dropChance.floatValue());
+
+				break;
+
+			case CHEST:
+				equip.setChestplate(item);
+
+				if (dropChance != null)
+					equip.setChestplateDropChance(dropChance.floatValue());
+
+				break;
+
+			case LEGS:
+				equip.setLeggings(item);
+
+				if (dropChance != null)
+					equip.setLeggingsDropChance(dropChance.floatValue());
+
+				break;
+
+			case FEET:
+				equip.setBoots(item);
+
+				if (dropChance != null)
+					equip.setBootsDropChance(dropChance.floatValue());
+
+				break;
+		}
+	}
+
+	/**
+	 * Attempts to parse equip. slot from the given key, or throwing
+	 * an error if not found
+	 *
+	 * @param key
+	 * @return
+	 */
+	public static CompEquipmentSlot fromKey(String key) {
+		for (final CompEquipmentSlot slot : values())
+			if (slot.key.equalsIgnoreCase(key.replace(" ", "_")))
+				return slot;
+
+		throw new FoException("No such equipment slot from key: " + key);
+	}
+
+	@Override
+	public String toString() {
+		return this.key.toUpperCase();
+	}
+}
