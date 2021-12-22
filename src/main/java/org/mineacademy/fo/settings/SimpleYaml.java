1d3580bcb8c124b7ca00246f5ebbce2bc3e85cd4
Matej Pacan <matej@matejpacan.com>
2021-12-22 09:13:17 +0100

    Remove SimpleYaml#getMap since it is unused and broke on 1.18.1

--- src/main/java/org/mineacademy/fo/settings/SimpleYaml.java
+++ src/main/java/org/mineacademy/fo/settings/SimpleYaml.java
@@ -70,17 +70,6 @@ public final class SimpleYaml extends FileConfiguration {
 		this.map.clear();
 	}
 
-	/**
-	 * Return the internal map with all value-key pairs stored in the memory
-	 *
-	 * @deprecated potentially dangerous
-	 * @return
-	 */
-	@Deprecated
-	public Map<String, Object> getMap() {
-		return this.map;
-	}
-
 	@Override
 	public String saveToString() {
 		return this.saveToString(getValues(false));
