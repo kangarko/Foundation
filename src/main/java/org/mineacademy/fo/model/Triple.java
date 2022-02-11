5c1c564bd45837da56e9006682ca9e322df121d1
Matej Pacan <matej@matejpacan.com>
2022-02-11 06:41:02 +0100

    Fix equals and hashcode not generating for Triple

--- src/main/java/org/mineacademy/fo/model/Triple.java
+++ src/main/java/org/mineacademy/fo/model/Triple.java
@@ -1,7 +1,6 @@
 package org.mineacademy.fo.model;
 
-import lombok.Getter;
-import lombok.RequiredArgsConstructor;
+import lombok.Data;
 
 /**
  * A simple placeholder for storing three values
@@ -10,8 +9,7 @@ import lombok.RequiredArgsConstructor;
  * @param <B>
  * @param <C>
  */
-@Getter
-@RequiredArgsConstructor
+@Data
 public final class Triple<A, B, C> {
 
 	/**
