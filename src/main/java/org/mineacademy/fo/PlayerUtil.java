ff22d9ee9dbef41ef984c87a8be44bbf5a143ee4
kangarko <matej@matejpacan.com>
2023-07-04 18:28:12 +0200

    Fix error in PlayerUtil#hasEmptyInventory

--- src/main/java/org/mineacademy/fo/PlayerUtil.java
+++ src/main/java/org/mineacademy/fo/PlayerUtil.java
@@ -531,11 +531,12 @@ public final class PlayerUtil {
 		final ItemStack[] inv = player.getInventory().getContents();
 		final ItemStack[] armor = player.getInventory().getArmorContents();
 
-		final ItemStack[] everything = (ItemStack[]) Common.joinArrays(inv, armor);
+		final Object[] everything = Common.joinArrays(inv, armor);
 
-		for (final ItemStack i : everything)
-			if (i != null && i.getType() != Material.AIR)
-				return false;
+		for (final Object i : everything)
+			if (i instanceof ItemStack)
+				if (((ItemStack) i).getType() != Material.AIR)
+					return false;
 
 		return true;
 	}
