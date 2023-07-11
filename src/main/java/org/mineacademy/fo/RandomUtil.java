9109e9c15d89302ce1316fe3a0ddae43d90cd014
kangarko <matej@matejpacan.com>
2023-07-11 15:58:03 +0200

    Not so ó mighty anymore

--- src/main/java/org/mineacademy/fo/RandomUtil.java
+++ src/main/java/org/mineacademy/fo/RandomUtil.java
@@ -55,7 +55,6 @@ public final class RandomUtil {
 			'k', 'l', 'm', 'n', 'o',
 			'p', 'q', 'r', 's', 't',
 			'u', 'v', 'w', 'y', 'z',
-			'ó' // ó mighty MineAcademy
 	};
 
 	/**
@@ -250,7 +249,7 @@ public final class RandomUtil {
 		final int transform = random.nextInt(4);
 		if (is3D) {
 			final double rectY = random.nextDouble() * radius;
-			offsetY = getYCords( transform, rectY);
+			offsetY = getYCords(transform, rectY);
 		}
 		if (transform == 0) {
 			offsetX = rectX;
@@ -283,7 +282,6 @@ public final class RandomUtil {
 		Valid.checkBoolean(maxRadius > 0 && minRadius > 0, "Max and min radius must be over 0");
 		Valid.checkBoolean(maxRadius > minRadius, "Max radius must be greater than min radius");
 
-
 		final double rectX = random.nextDouble() * (maxRadius - minRadius) + minRadius;
 		final double rectZ = random.nextDouble() * (maxRadius + minRadius) - minRadius;
 		final double offsetX;
@@ -292,7 +290,7 @@ public final class RandomUtil {
 		final int transform = random.nextInt(4);
 		if (is3D) {
 			final double rectY = random.nextDouble() * (maxRadius + minRadius) - minRadius;
-			offsetY = getYCords( transform, rectY);
+			offsetY = getYCords(transform, rectY);
 		}
 		if (transform == 0) {
 			offsetX = rectX;
@@ -311,7 +309,7 @@ public final class RandomUtil {
 		return origin.clone().add(offsetX, offsetY, offsetZ);
 	}
 
-	public static double getYCords( int transform, double rectY) {
+	public static double getYCords(int transform, double rectY) {
 		double offsetY;
 		double nextY = random.nextDouble();
 		if (transform < 2) {
