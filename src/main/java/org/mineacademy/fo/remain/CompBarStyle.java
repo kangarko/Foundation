9099035745bbf2b8db64b6703d0a251be51f2111
MrCrazys <73004328+MrCrazys@users.noreply.github.com>
2023-07-21 15:46:10 +0800

    Update CompBarStyle.java
    Added a link to Bukkit's `BarStyle` class
    
    Moved `@Getter` annotation to the top of the class to avoid repetition
    
    Added JavaDocs for the fields
    
    Added JavaDoc to the `toString()` method

--- src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
+++ src/main/java/org/mineacademy/fo/remain/CompBarStyle.java
@@ -1,63 +1,73 @@
 package org.mineacademy.fo.remain;
 
-import org.mineacademy.fo.Common;
-
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
+import org.bukkit.boss.BarStyle;
+import org.mineacademy.fo.Common;
 
 /**
- * A wrapper class for BarStyle from Bukkit.
+ * A wrapper class for {@link BarStyle} from Bukkit.
  */
+@Getter
 @RequiredArgsConstructor
 public enum CompBarStyle {
 
-	/**
-	 * Makes the boss bar solid (no segments).
-	 */
-	SOLID("SOLID", "SOLID"),
+    /**
+     * Makes the boss bar solid (no segments).
+     */
+    SOLID("SOLID", "SOLID"),
 
-	/**
-	 * Splits the boss bar into 6 segments.
-	 */
-	SEGMENTED_6("SEGMENTED_6", "SEG6"),
+    /**
+     * Splits the boss bar into 6 segments.
+     */
+    SEGMENTED_6("SEGMENTED_6", "SEG6"),
 
-	/**
-	 * Splits the boss bar into 10 segments.
-	 */
-	SEGMENTED_10("SEGMENTED_10", "SEG10"),
+    /**
+     * Splits the boss bar into 10 segments.
+     */
+    SEGMENTED_10("SEGMENTED_10", "SEG10"),
 
-	/**
-	 * Splits the boss bar into 12 segments.
-	 */
-	SEGMENTED_12("SEGMENTED_12", "SEG12"),
+    /**
+     * Splits the boss bar into 12 segments.
+     */
+    SEGMENTED_12("SEGMENTED_12", "SEG12"),
 
-	/**
-	 * Splits the boss bar into 20 segments.
-	 */
-	SEGMENTED_20("SEGMENTED_20", "SEG20");
+    /**
+     * Splits the boss bar into 20 segments.
+     */
+    SEGMENTED_20("SEGMENTED_20", "SEG20");
 
-	@Getter
-	private final String key;
+    /**
+     * The internal key for this bar style.
+     */
+    private final String key;
 
-	@Getter
-	private final String shortKey;
+    /**
+     * The short key for this bar style.
+     */
+    private final String shortKey;
 
-	/**
-	 * Attempts to load a CompBarStyle from the given key.
-	 *
-	 * @param key
-	 * @return
-	 */
-	public static CompBarStyle fromKey(String key) {
-		for (final CompBarStyle mode : values())
-			if (mode.key.equalsIgnoreCase(key) || mode.shortKey.equalsIgnoreCase(key))
-				return mode;
+    /**
+     * Attempts to load a bar style from the given key.
+     *
+     * @param key the key to load.
+     * @return the found bar style.
+     */
+    public static CompBarStyle fromKey(final String key) {
+        for (final CompBarStyle style : values())
+            if (style.key.equalsIgnoreCase(key) || style.shortKey.equalsIgnoreCase(key))
+                return style;
 
-		throw new IllegalArgumentException("No such CompBarStyle: " + key + ". Available: " + Common.join(values()));
-	}
+        throw new IllegalArgumentException("No such CompBarStyle: " + key + ". Available: " + Common.join(values()));
+    }
 
-	@Override
-	public String toString() {
-		return this.key;
-	}
+    /**
+     * Returns the internal key for this bar style.
+     *
+     * @return the internal key for this bar style.
+     */
+    @Override
+    public String toString() {
+        return this.key;
+    }
 }
