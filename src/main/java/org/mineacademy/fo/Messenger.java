419191bbcdb33f6f83c6f32ee97e04e9a2f92f87
MrCrazys <archie.harrigan@outlook.com.au>
2023-10-06 08:05:30 +0800

    Reformat * Replaced UtilityClass annotation with NoArgsConstructor annotation and added static to all methods. This is just for consistency with the other classes, and also to make it clear. * Changed JavaDoc.

--- src/main/java/org/mineacademy/fo/Messenger.java
+++ src/main/java/org/mineacademy/fo/Messenger.java
@@ -1,203 +1,207 @@
 package org.mineacademy.fo;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
 import org.mineacademy.fo.model.Replacer;
 import org.mineacademy.fo.remain.Remain;
 import org.mineacademy.fo.settings.SimpleSettings;
 
-import lombok.Getter;
-import lombok.Setter;
-import lombok.experimental.UtilityClass;
-
 /**
- * Streamlines the process of sending themed messages to players
+ * Streamlines the process of sending themed messages to players.
  */
-@UtilityClass
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class Messenger {
 
 	/**
-	 * Should we use messenger globally such as in commands & listeners?
+	 * Should we use messenger globally such as in commands and listeners?
 	 */
 	public static boolean ENABLED = true;
 
 	/**
-	 * The prefix send while sending info message
+	 * The prefix sent before info messages.
 	 */
 	@Setter
 	@Getter
-	private String infoPrefix = "&8&l[&9&li&8&l]&7 ";
+	private static String infoPrefix = "&8&l[&9&li&8&l]&7 ";
 
 	/**
-	 * The prefix send while sending success message
+	 * The prefix sent before success messages.
 	 */
 	@Setter
 	@Getter
-	private String successPrefix = "&8&l[&2&l\u2714&8&l]&7 ";
+	private static String successPrefix = "&8&l[&2&l\u2714&8&l]&7 ";
 
 	/**
-	 * The prefix send while sending warning message
+	 * The prefix sent before warning messages.
 	 */
 	@Setter
 	@Getter
-	private String warnPrefix = "&8&l[&6&l!&8&l]&6 ";
+	private static String warnPrefix = "&8&l[&6&l!&8&l]&6 ";
 
 	/**
-	 * The prefix send while sending error message
+	 * The prefix sent before error messages.
 	 */
 	@Setter
 	@Getter
-	private String errorPrefix = "&8&l[&4&l\u2715&8&l]&c ";
+	private static String errorPrefix = "&8&l[&4&l\u2715&8&l]&c ";
 
 	/**
-	 * The prefix send while sending questions
+	 * The prefix sent before questions.
 	 */
 	@Setter
 	@Getter
-	private String questionPrefix = "&8&l[&a&l?&l&8]&7 ";
+	private static String questionPrefix = "&8&l[&a&l?&l&8]&7 ";
 
 	/**
-	 * The prefix send while sending announcements
+	 * The prefix sent before announcements.
 	 */
 	@Setter
 	@Getter
-	private String announcePrefix = "&8&l[&5&l!&l&8]&d ";
+	private static String announcePrefix = "&8&l[&5&l!&l&8]&d ";
 
 	/**
-	 * Send a message prepended with the {@link #getInfoPrefix()}
+	 * Broadcasts a message prepended with {@link #getInfoPrefix()}
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastInfo(final String message) {
+	public static void broadcastInfo(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, infoPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getSuccessPrefix()}
+	 * Broadcasts a message prepended with {@link #getSuccessPrefix()}.
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastSuccess(final String message) {
+	public static void broadcastSuccess(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, successPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getWarnPrefix()}
+	 * Broadcasts a message prepended with {@link #getWarnPrefix()}.
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastWarn(final String message) {
+	public static void broadcastWarn(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, warnPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getErrorPrefix()}
+	 * Broadcasts a message prepended with {@link #getErrorPrefix()}.
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastError(final String message) {
+	public static void broadcastError(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, errorPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getQuestionPrefix()}
+	 * Broadcasts a message prepended with {@link #getQuestionPrefix()}.
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastQuestion(final String message) {
+	public static void broadcastQuestion(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, questionPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getAnnouncePrefix()}
+	 * Broadcasts a message prepended with {@link #getAnnouncePrefix()}.
 	 *
-	 * @param message
+	 * @param message the message to broadcast.
 	 */
-	public void broadcastAnnounce(final String message) {
+	public static void broadcastAnnounce(final String message) {
 		for (final Player online : Remain.getOnlinePlayers())
 			tell(online, announcePrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getInfoPrefix()}
+	 * Sends a message prepended with {@link #getInfoPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void info(final CommandSender player, final String message) {
+	public static void info(final CommandSender player, final String message) {
 		tell(player, infoPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getSuccessPrefix()}
+	 * Sends a message prepended with {@link #getSuccessPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void success(final CommandSender player, final String message) {
+	public static void success(final CommandSender player, final String message) {
 		tell(player, successPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getWarnPrefix()}
+	 * Sends a message prepended with {@link #getWarnPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void warn(final CommandSender player, final String message) {
+	public static void warn(final CommandSender player, final String message) {
 		tell(player, warnPrefix, message);
 	}
 
 	/**
-	 * Send messages prepended with the {@link #getErrorPrefix()}
+	 * Sends messages prepended with {@link #getErrorPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param messages
+	 * @param player   the {@link CommandSender} to send the message to.
+	 * @param messages the messages to send.
 	 */
-	public void error(final CommandSender player, final String... messages) {
+	public static void error(final CommandSender player, final String... messages) {
 		for (final String message : messages)
 			error(player, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getErrorPrefix()}
+	 * Sends a message prepended with {@link #getErrorPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void error(final CommandSender player, final String message) {
+	public static void error(final CommandSender player, final String message) {
 		tell(player, errorPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getQuestionPrefix()}
+	 * Sends a message prepended with {@link #getQuestionPrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void question(final CommandSender player, final String message) {
+	public static void question(final CommandSender player, final String message) {
 		tell(player, questionPrefix, message);
 	}
 
 	/**
-	 * Send a message prepended with the {@link #getAnnouncePrefix()}
+	 * Sends a message prepended with {@link #getAnnouncePrefix()} to the given {@link CommandSender}.
 	 *
-	 * @param player
-	 * @param message
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param message the message to send.
 	 */
-	public void announce(final CommandSender player, final String message) {
+	public static void announce(final CommandSender player, final String message) {
 		tell(player, announcePrefix, message);
 	}
 
-	/*
-	 * Internal method to perform the sending
+	/**
+	 * Sends a message to the given {@link CommandSender}.
+	 *
+	 * @param player  the {@link CommandSender} to send the message to.
+	 * @param prefix  the prefix to prepend to the message.
+	 * @param message the message to send.
 	 */
-	private void tell(final CommandSender player, final String prefix, String message) {
+	private static void tell(final CommandSender player, final String prefix, String message) {
 
 		// Support localization being none or empty
 		if (message.isEmpty() || "none".equals(message))
@@ -206,7 +210,7 @@ public class Messenger {
 		final String colorless = Common.stripColors(message);
 		boolean noPrefix = ChatUtil.isInteractive(colorless);
 
-		// Special case: Send the prefix for actionbar
+		// Special case: Send the prefix for actionbar.
 		if (colorless.startsWith("<actionbar>"))
 			message = message.replace("<actionbar>", "<actionbar>" + prefix);
 
@@ -216,18 +220,18 @@ public class Messenger {
 			noPrefix = true;
 		}
 
-		// Only insert prefix if the message is sent through the normal chat
+		// Only insert the prefix if the message is sent through the normal chat.
 		Common.tellNoPrefix(player, (noPrefix ? "" : prefix) + message);
 	}
 
 	/**
-	 * Replace {plugin_prefix} and {X_prefix} and {prefix_X} with respective messenger variables
-	 * such as {warn_prefix} with {@link #getWarnPrefix()} etc.
+	 * Replaces {plugin_prefix}, {X_prefix} and {prefix_X} with respective messenger variables such as {warn_prefix}
+	 * with {@link #getWarnPrefix()} etc.
 	 *
-	 * @param message
-	 * @return
+	 * @param message the message to replace prefixes in.
+	 * @return the message with prefixes replaced.
 	 */
-	public static String replacePrefixes(String message) {
+	public static String replacePrefixes(final String message) {
 		return Replacer.replaceArray(message,
 				"plugin_prefix", SimpleSettings.PLUGIN_PREFIX,
 				"info_prefix", infoPrefix,
