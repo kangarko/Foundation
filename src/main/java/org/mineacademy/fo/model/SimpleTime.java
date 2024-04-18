4c23276d038448eb466d83e50024a080618d983d
Matej Pacan <matej@matejpacan.com>
2024-04-18 20:55:08 +0200

    Improve and add new helper methods to SimpleTime

--- src/main/java/org/mineacademy/fo/model/SimpleTime.java
+++ src/main/java/org/mineacademy/fo/model/SimpleTime.java
@@ -26,27 +26,6 @@ public final class SimpleTime {
 	}
 
 	/**
-	 * Generate new time from the given seconds
-	 *
-	 * @param seconds
-	 * @return
-	 */
-	public static SimpleTime fromSeconds(final int seconds) {
-		return from(seconds + " seconds");
-	}
-
-	/**
-	 * Generate new time. Valid examples: 15 ticks 1 second 25 minutes 3 hours etc.
-	 * or input "none" to create an instance with the time of 0
-	 *
-	 * @param time
-	 * @return
-	 */
-	public static SimpleTime from(final String time) {
-		return new SimpleTime(time);
-	}
-
-	/**
 	 * Get the time specified in seconds (ticks / 20)
 	 *
 	 * @return
@@ -84,15 +63,69 @@ public final class SimpleTime {
 	}
 
 	/**
+	 * Return true if the given limit has already passed from the current timestamp
+	 *
+	 * @param limitMs
+	 * @return
+	 */
+	public boolean isOverLimitMs(final long limitMs) {
+		return (System.currentTimeMillis() - limitMs) > this.getTimeMilliseconds();
+	}
+
+	/**
+	 * Return true if the given limit has not yet passed from the current timestamp
+	 *
+	 * @param limitMs
+	 * @return
+	 */
+	public boolean isUnderLimitMs(final long limitMs) {
+		return (System.currentTimeMillis() - limitMs) < this.getTimeMilliseconds();
+	}
+
+	/**
+	 * Format the time left until the given limit in human readable form
+	 *
+	 * @param lastLastExecutionMs
+	 * @return
+	 */
+	public String formatWaitTime(final long lastLastExecutionMs) {
+		final long limit = this.getTimeMilliseconds();
+		final long delay = System.currentTimeMillis() - lastLastExecutionMs;
+
+		return TimeUtil.formatTimeGeneric(1 + (limit - delay) / 1000L);
+	}
+
+	/**
 	 * @see java.lang.Object#equals(java.lang.Object)
 	 */
 	@Override
-	public boolean equals(Object obj) {
+	public boolean equals(final Object obj) {
 		return obj instanceof SimpleTime && ((SimpleTime) obj).timeTicks == this.timeTicks;
 	}
 
 	@Override
 	public String toString() {
 		return this.getRaw();
+	}
+
+	/**
+	 * Generate new time from the given seconds
+	 *
+	 * @param seconds
+	 * @return
+	 */
+	public static SimpleTime fromSeconds(final int seconds) {
+		return from(seconds + " seconds");
+	}
+
+	/**
+	 * Generate new time. Valid examples: 15 ticks 1 second 25 minutes 3 hours etc.
+	 * or input "none" to create an instance with the time of 0
+	 *
+	 * @param time
+	 * @return
+	 */
+	public static SimpleTime from(final String time) {
+		return new SimpleTime(time);
 	}
 }
