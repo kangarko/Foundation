f78ca480e7d079545a06d2a4664df8b4914e79c2
Matej Pacan <matej@matejpacan.com>
2024-07-10 17:22:56 +0200

    Vastly improve network util
    Allow request params
    Better error handling

--- src/main/java/org/mineacademy/fo/NetworkUtil.java
+++ src/main/java/org/mineacademy/fo/NetworkUtil.java
@@ -1,8 +1,8 @@
 package org.mineacademy.fo;
 
 import java.io.BufferedReader;
-import java.io.DataOutputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.URL;
 import java.net.URLConnection;
@@ -24,6 +24,11 @@ import lombok.NoArgsConstructor;
 public final class NetworkUtil {
 
 	/**
+	 * Mask user agent to Chrome 126 by default, you can override this in your request properties.
+	 */
+	private final static String USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36";
+
+	/**
 	 * Makes a GET request to the specified endpoint and returns the response as a JSON object.
 	 *
 	 * @param endpoint The URL to send the GET request to.
@@ -37,24 +42,53 @@ public final class NetworkUtil {
 	 * Makes a GET request to the specified endpoint with the given parameters and returns the response as a JSON object.
 	 *
 	 * @param endpoint The URL to send the GET request to.
-	 * @param params   A map of query parameters to include in the request.
-	 * @return A JSONObject containing the response, or null if the request or parsing fails.
+	 * @param params A map of query parameters to include in the request.
+	 * @return
 	 */
 	public static JSONObject getJson(String endpoint, SerializedMap params) {
-		final String response = get(endpoint, params);
+		return getJson(endpoint, params, null);
+	}
+
+	/**
+	 * Makes a GET request to the specified endpoint with the given parameters and returns the response as a JSON object.
+	 *
+	 * @param endpoint The URL to send the GET request to.
+	 * @param params   A map of query parameters to include in the request.
+	 * @param requestProperties The request properties to include in the request, null to ignore.
+	 *
+	 * @return A JSONObject containing the response, or null if the request or parsing fails.
+	 */
+	public static JSONObject getJson(String endpoint, SerializedMap params, SerializedMap requestProperties) {
+		final String response = get(endpoint, params, requestProperties);
 
 		try {
-			return (JSONObject) JSONParser.deserialize(response);
+			final Object json = JSONParser.deserialize(response);
+
+			try {
+				return (JSONObject) json;
+
+			} catch (final ClassCastException ex) {
+				Common.throwError(ex,
+						"Failed to cast JSON to JSONObject!",
+						"",
+						"Endpoint: " + endpoint,
+						"Params: " + params,
+						"Request Properties: " + requestProperties,
+						"Response: " + response,
+						"Raw JSON (" + json.getClass().getSimpleName() + "): " + json);
+			}
 
 		} catch (final JSONParseException ex) {
 			Common.throwError(ex,
-					"Failed to get JSON! ",
+					"Failed to get JSON!",
+					"",
 					"Endpoint: " + endpoint,
 					"Params: " + params,
+					"Request Properties: " + requestProperties,
 					"Response: " + response);
-
-			return null;
 		}
+
+		return null;
 	}
 
 	/**
@@ -71,24 +105,53 @@ public final class NetworkUtil {
 	 * Makes a POST request to the specified endpoint with the given parameters and returns the response as a JSON object.
 	 *
 	 * @param endpoint The URL to send the POST request to.
-	 * @param params  A map of parameters to include in the request.
-	 * @return A JSONObject containing the response, or null if the request or parsing fails.
+	 * @param params A map of parameters to include in the request.
+	 * @return
 	 */
 	public static JSONObject postJson(String endpoint, SerializedMap params) {
-		final String response = post(endpoint, params);
+		return postJson(endpoint, params, null);
+	}
+
+	/**
+	 * Makes a POST request to the specified endpoint with the given parameters and returns the response as a JSON object.
+	 *
+	 * @param endpoint The URL to send the POST request to.
+	 * @param params  A map of parameters to include in the request.
+	 * @param requestProperties The request properties to include in the request, null to ignore.
+	 *
+	 * @return A JSONObject containing the response, or null if the request or parsing fails.
+	 */
+	public static JSONObject postJson(String endpoint, SerializedMap params, SerializedMap requestProperties) {
+		final String response = post(endpoint, params, requestProperties);
 
 		try {
-			return (JSONObject) JSONParser.deserialize(response);
+			final Object json = JSONParser.deserialize(response);
+
+			try {
+				return (JSONObject) json;
+
+			} catch (final ClassCastException ex) {
+				Common.throwError(ex,
+						"Failed to cast JSON to JSONObject!",
+						"",
+						"Endpoint: " + endpoint,
+						"Params: " + params,
+						"Request Properties: " + requestProperties,
+						"Response: " + response,
+						"Raw JSON (" + json.getClass().getSimpleName() + "): " + json);
+			}
 
 		} catch (final JSONParseException ex) {
 			Common.throwError(ex,
-					"Failed to post JSON! ",
+					"Failed to post JSON!",
+					"",
 					"Endpoint: " + endpoint,
 					"Params: " + params,
+					"Request Properties: " + requestProperties,
 					"Response: " + response);
-
-			return null;
 		}
+
+		return null;
 	}
 
 	/**
@@ -105,10 +168,23 @@ public final class NetworkUtil {
 	 * Makes a GET request to the specified endpoint with the given parameters and returns the response as a string.
 	 *
 	 * @param endpoint The URL to send the GET request to.
-	 * @param params   A map of query parameters to include in the request.
-	 * @return A string containing the response, or an empty string if the request fails.
+	 * @param params A map of query parameters to include in the request, null to ignore.
+	 * @return
 	 */
 	public static String get(String endpoint, SerializedMap params) {
+		return get(endpoint, params, null);
+	}
+
+	/**
+	 * Makes a GET request to the specified endpoint with the given parameters and returns the response as a string.
+	 *
+	 * @param endpoint The URL to send the GET request to.
+	 * @param params   A map of query parameters to include in the request, null to ignore.
+	 * @param requestProperties The request properties to include in the request, null to ignore.
+	 *
+	 * @return A string containing the response, or an empty string if the request fails.
+	 */
+	public static String get(String endpoint, SerializedMap params, SerializedMap requestProperties) {
 
 		if (params == null)
 			params = new SerializedMap();
@@ -129,9 +205,11 @@ public final class NetworkUtil {
 			final URL url = new URL(endpoint);
 			final URLConnection connection = url.openConnection();
 
-			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
-			connection.setConnectTimeout(3000);
-			connection.setReadTimeout(3000);
+			connection.setRequestProperty("User-Agent", USER_AGENT);
+
+			if (requestProperties != null)
+				for (final Map.Entry<String, Object> entry : requestProperties.entrySet())
+					connection.setRequestProperty(entry.getKey(), entry.getValue().toString());
 
 			try (final BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
 				final StringBuilder responseBuilder = new StringBuilder();
@@ -164,10 +242,23 @@ public final class NetworkUtil {
 	 * Makes a POST request to the specified endpoint with the given parameters and returns the response as a string.
 	 *
 	 * @param endpoint The URL to send the POST request to.
-	 * @param params   A map of parameters to include in the request.
-	 * @return A string containing the response, or an empty string if the request fails.
+	 * @param params A map of parameters to include in the request, null to ignore.
+	 * @return
 	 */
 	public static String post(String endpoint, SerializedMap params) {
+		return post(endpoint, params, null);
+	}
+
+	/**
+	 * Makes a POST request to the specified endpoint with the given parameters and returns the response as a string.
+	 *
+	 * @param endpoint The URL to send the POST request to.
+	 * @param params   A map of parameters to include in the request, null to ignore.
+	 * @param requestProperties The request properties to include in the request, null to ignore.
+	 *
+	 * @return A string containing the response, or an empty string if the request fails.
+	 */
+	public static String post(String endpoint, SerializedMap params, SerializedMap requestProperties) {
 
 		if (params == null)
 			params = new SerializedMap();
@@ -181,18 +272,20 @@ public final class NetworkUtil {
 
 			connection.setRequestMethod("POST");
 			connection.setDoOutput(true);
-			connection.setConnectTimeout(3000);
-			connection.setReadTimeout(3000);
+
 			connection.setRequestProperty("Content-Type", "application/json");
-			connection.setRequestProperty("User-Agent", "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/126.0.0.0 Safari/537.36");
+			connection.setRequestProperty("User-Agent", USER_AGENT);
 
-			if (params != null && !params.isEmpty()) {
-				final byte[] postDataBytes = params.toJson().getBytes("UTF-8");
+			if (requestProperties != null)
+				for (final Map.Entry<String, Object> entry : requestProperties.entrySet())
+					connection.setRequestProperty(entry.getKey(), entry.getValue().toString());
 
-				try (DataOutputStream wr = new DataOutputStream(connection.getOutputStream())) {
-					wr.write(postDataBytes);
+			if (params != null && !params.isEmpty())
+				try (OutputStream output = connection.getOutputStream()) {
+					final byte[] input = params.toJson().getBytes("utf-8");
+
+					output.write(input, 0, input.length);
 				}
-			}
 
 			try (BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
 				final StringBuilder responseBuilder = new StringBuilder();
