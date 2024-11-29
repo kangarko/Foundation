3a0373acffe218a63be447ab941202412a0f0dc0
Matej Pacan <matej@matejpacan.com>
2024-11-29 06:49:06 +0100

    Update CommonCore.java

--- foundation-core/src/main/java/org/mineacademy/fo/CommonCore.java
+++ foundation-core/src/main/java/org/mineacademy/fo/CommonCore.java
@@ -786,7 +786,6 @@ public abstract class CommonCore {
 	 * @return A Pattern object that represents the compiled regular expression with applied platform settings.
 	 */
 	public static final Pattern compilePattern(String regex) {
-		regex = Platform.getPlugin().isRegexStrippingColors() ? CompChatColor.stripColorCodes(regex) : regex;
 		regex = Platform.getPlugin().isRegexStrippingAccents() ? ChatUtil.replaceDiacritic(regex) : regex;
 
 		if (Platform.getPlugin().isRegexCaseInsensitive())
