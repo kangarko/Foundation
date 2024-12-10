fed9e707ab0db9a9da551ac0ec7eb3ea9ffd29d5
Matej Pacan <matej@matejpacan.com>
2024-12-10 16:16:26 +0100

    Some plugins are bad

--- foundation-core/src/main/java/org/mineacademy/fo/model/SimpleComponent.java
+++ foundation-core/src/main/java/org/mineacademy/fo/model/SimpleComponent.java
@@ -63,14 +63,29 @@ public final class SimpleComponent implements ConfigSerializable {
 	 * prevents removing the second tag from \<red\>hello {player}\<red\>
 	 * which makes colored placeholders revert back properly.
 	 */
-	public static final MiniMessage MINIMESSAGE_PARSER = MiniMessage
-			.builder()
-			.tags(TagResolver.standard())
-			.strict(false)
-			.preProcessor(UnaryOperator.identity())
-			.postProcessor(UnaryOperator.identity())
-			.debug(null)
-			.build();
+	public static final MiniMessage MINIMESSAGE_PARSER;
+
+	static {
+		try {
+			TextDecoration.class.getMethod("withState", boolean.class);
+
+		} catch (final ReflectiveOperationException err) {
+			CommonCore.logFramed(
+					"Fatal error initializing MiniMessage. If you see",
+					"plugin name below, report this to its developer",
+					"because this is a bug in their plugin by shading",
+					"MiniMessage inside their jar without relocating it");
+		}
+
+		MINIMESSAGE_PARSER = MiniMessage
+				.builder()
+				.tags(TagResolver.standard())
+				.strict(false)
+				.preProcessor(UnaryOperator.identity())
+				.postProcessor(UnaryOperator.identity())
+				.debug(null)
+				.build();
+	}
 
 	/**
 	 * The empty component
