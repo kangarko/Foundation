2688ac4e96e028d47183721257cef36cdc044aae
Matej Pacan <matej@matejpacan.com>
2024-12-16 17:35:56 +0100

    Properly strip tags before checking if legacy component hover should split lines

--- foundation-core/src/main/java/org/mineacademy/fo/model/SimpleComponent.java
+++ foundation-core/src/main/java/org/mineacademy/fo/model/SimpleComponent.java
@@ -195,7 +195,7 @@ public final class SimpleComponent implements ConfigSerializable {
 			legacy = CompChatColor.convertMiniToLegacy("<gray>" + CompChatColor.translateColorCodes(legacy));
 
 			// Receiver conditions and hover/click (unsupported) tags will be lost
-			if (MinecraftVersion.hasVersion() && MinecraftVersion.olderThan(V.v1_13) && legacy.length() > LEGACY_HOVER_LINE_LENGTH_LIMIT)
+			if (MinecraftVersion.hasVersion() && MinecraftVersion.olderThan(V.v1_13) && MiniMessage.miniMessage().stripTags(legacy).length() > LEGACY_HOVER_LINE_LENGTH_LIMIT)
 				legacy = String.join("\n", CommonCore.split(legacy, LEGACY_HOVER_LINE_LENGTH_LIMIT));
 
 			// This is up to 1.5-2x faster
