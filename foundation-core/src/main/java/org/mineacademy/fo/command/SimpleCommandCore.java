d40dd2209dd58c6f2336e19f0e4fc7cac57776ba
Matej Pacan <matej@matejpacan.com>
2024-12-19 20:07:11 +0100

    Format

--- foundation-core/src/main/java/org/mineacademy/fo/command/SimpleCommandCore.java
+++ foundation-core/src/main/java/org/mineacademy/fo/command/SimpleCommandCore.java
@@ -421,7 +421,7 @@ public abstract class SimpleCommandCore {
 
 		// Check if the command was run earlier within the wait threshold
 		if (lastRun != 0)
-			this.checkBoolean(difference > this.cooldownSeconds, CommonCore.getOrDefault(this.cooldownMessage, Lang.component("command-cooldown-wait")).replaceBracket( "duration", String.valueOf(this.cooldownSeconds - difference + 1)));
+			this.checkBoolean(difference > this.cooldownSeconds, CommonCore.getOrDefault(this.cooldownMessage, Lang.component("command-cooldown-wait")).replaceBracket("duration", String.valueOf(this.cooldownSeconds - difference + 1)));
 
 		// Update the last try with the current time
 		this.lastExecutedTimes.put(this.audience.getName(), System.currentTimeMillis());
@@ -473,7 +473,7 @@ public abstract class SimpleCommandCore {
 	 */
 	protected final void checkPerm(@NonNull final String permission) throws CommandException {
 		if (!this.hasPerm(permission))
-			throw new CommandException(this.getPermissionMessage().replaceBracket( "permission", permission));
+			throw new CommandException(this.getPermissionMessage().replaceBracket("permission", permission));
 	}
 
 	/**
@@ -485,7 +485,7 @@ public abstract class SimpleCommandCore {
 	 */
 	protected final void checkPerm(@NonNull final FoundationPlayer audience, @NonNull final String permission) throws CommandException {
 		if (!audience.hasPermission(permission))
-			throw new CommandException(this.getPermissionMessage().replaceBracket( "permission", permission));
+			throw new CommandException(this.getPermissionMessage().replaceBracket("permission", permission));
 	}
 
 	/**
@@ -718,9 +718,9 @@ public abstract class SimpleCommandCore {
 		}
 
 		this.checkNotNull(found, falseMessage
-				.replaceBracket( "type", enumType.getSimpleName().replaceAll("([a-z])([A-Z]+)", "$1 $2").toLowerCase())
-				.replaceBracket( "value", enumValue)
-				.replaceBracket( "available", CommonCore.join(Arrays.asList(ReflectionUtil.getEnumValues(enumType))
+				.replaceBracket("type", enumType.getSimpleName().replaceAll("([a-z])([A-Z]+)", "$1 $2").toLowerCase())
+				.replaceBracket("value", enumValue)
+				.replaceBracket("available", CommonCore.join(Arrays.asList(ReflectionUtil.getEnumValues(enumType))
 						.stream()
 						.filter(listConst -> condition == null || condition.apply(listConst))
 						.collect(Collectors.toList()),
@@ -826,7 +826,7 @@ public abstract class SimpleCommandCore {
 	 * You can use {min} and {max} in the message to be automatically replaced
 	 */
 	private final <T extends Number & Comparable<T>> T findNumber(final Class<T> numberType, final int index, final T min, final T max, SimpleComponent falseMessage) {
-		falseMessage = falseMessage.replaceBracket( "min", String.valueOf(min)).replaceBracket( "max", String.valueOf(max));
+		falseMessage = falseMessage.replaceBracket("min", String.valueOf(min)).replaceBracket("max", String.valueOf(max));
 
 		final T number = this.findNumber(numberType, index, falseMessage);
 		this.checkBoolean(number.compareTo(min) >= 0 && number.compareTo(max) <= 0, falseMessage);
@@ -935,7 +935,7 @@ public abstract class SimpleCommandCore {
 			uuid = UUID.fromString(this.args[index]);
 
 		} catch (final IllegalArgumentException ex) {
-			this.returnTell(invalidMessage.replaceBracket( "uuid", this.args[index]));
+			this.returnTell(invalidMessage.replaceBracket("uuid", this.args[index]));
 		}
 
 		return uuid;
