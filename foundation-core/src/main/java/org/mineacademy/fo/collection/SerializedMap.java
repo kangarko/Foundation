592a9086d169cfaf9611b720f2fbe80eaa7d9db0
Matej Pacan <matej@matejpacan.com>
2025-01-22 20:58:32 +0100

    Do report a SerializedMap error to sentry

--- foundation-core/src/main/java/org/mineacademy/fo/collection/SerializedMap.java
+++ foundation-core/src/main/java/org/mineacademy/fo/collection/SerializedMap.java
@@ -593,7 +593,8 @@ public final class SerializedMap implements Iterable<Map.Entry<String, Object>> 
 				for (final Object object : (Object[]) rawList)
 					list.add(object == null ? null : SerializeUtilCore.deserialize(this.language, type, object, parameters));
 			else {
-				ValidCore.checkBoolean(rawList instanceof Collection<?>, "Key '" + key + "' expected to have a list, got " + rawList.getClass().getSimpleName() + " instead! Try putting '' quotes around the message: " + rawList);
+				if (!(rawList instanceof Collection<?>))
+					throw new FoException("Key '" + key + "' expected to have a list, got " + rawList.getClass().getSimpleName() + " instead! Try putting '' quotes around the message: " + rawList, false);
 
 				for (final Object object : (Collection<Object>) rawList)
 					list.add(object == null ? null : SerializeUtilCore.deserialize(this.language, type, object, parameters));
