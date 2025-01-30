4a8742333c0b301b850c21687a22348c25f16df5
Matej Pacan <matej@matejpacan.com>
2025-01-30 20:14:34 +0100

    Fix Button status colors

--- foundation-bukkit/src/main/java/org/mineacademy/fo/menu/button/Button.java
+++ foundation-bukkit/src/main/java/org/mineacademy/fo/menu/button/Button.java
@@ -267,7 +267,8 @@ public abstract class Button {
 				final ItemStack item = creator.glow(has).make();
 				final ItemMeta meta = item.getItemMeta();
 
-				meta.setLore(Variables.builder().placeholder("status", has ? "&aEnabled" : "&cDisabled").replaceLegacyList(meta.getLore()));
+				meta.setLore(Variables.builder().placeholder("status", has ? "§aEnabled" : "§cDisabled").replaceLegacyList(meta.getLore()));
+
 				item.setItemMeta(meta);
 
 				return item;
