b2ac14456176fbe6d32b7d7eda19ba36341f3c6e
kangarko <matej@matejpacan.com>
2026-04-09 07:03:20 +0200

    On 1.21 we dont need ProtocolLib for custom enchants

--- foundation-bukkit/src/main/java/org/mineacademy/fo/platform/BukkitPlugin.java
+++ foundation-bukkit/src/main/java/org/mineacademy/fo/platform/BukkitPlugin.java
@@ -353,7 +353,7 @@ public abstract class BukkitPlugin extends JavaPlugin implements Listener, Found
 
 							if (Bukkit.getPluginManager().getPlugin("ProtocolLib") != null)
 								BukkitEnchantPacketListener.getInstance().onRegister();
-							else
+							else if (MinecraftVersion.olderThan(V.v1_21))
 								CommonCore.warning("Custom enchantments require ProtocolLib for lore to be added properly.");
 						}
 
