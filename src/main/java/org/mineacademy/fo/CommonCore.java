b0b24babd2d2c97224ab75a8f65f61900c94017a
kangarko <matej@matejpacan.com>
2026-04-24 20:45:54 +0200

    Add shim classes to ease migration to v7

---
+++ src/main/java/org/mineacademy/fo/CommonCore.java
@@ -0,0 +1,8 @@
+package org.mineacademy.fo;
+
+/** 
+ * @deprecated A shim class for Foundation v7 compatibility. Use the Valid class instead.
+ */
+@Deprecated
+public final class CommonCore extends Common {
+}
---
+++ src/main/java/org/mineacademy/fo/ValidCore.java
@@ -0,0 +1,11 @@
+package org.mineacademy.fo;
+
+/**
+ * Utility class for checking conditions and throwing our safe exception that is
+ * logged into file.
+ * 
+ * @deprecated A shim class for Foundation v7 compatibility. Use the Valid class instead.
+ */
+@Deprecated
+public final class ValidCore extends Valid {
+}
